5b791b06395f60d30657134f3b1dd12aa17c8176
alxevvv <alxevvv@gmail.com>
2022-12-07 11:00:14 +0600

    navbar dropdown components

---
+++ src/components/layout/NavbarDropdown.vue
@@ -0,0 +1,32 @@
+<template>
+  <div
+    :class="{
+      'navbar-item': true,
+      'is-active': isActiveRoute
+    }"
+  >
+    <span><slot /></span>
+
+    <o-icon :icon="props.isActive ? 'angle-up' : 'angle-down'" />
+  </div>
+</template>
+
+<script setup>
+import { computed } from "vue";
+import { useRoute } from "vue-router";
+
+const props = defineProps({
+  isActive: {
+    type: Boolean,
+    required: true,
+  },
+  pagesNamePrefix: {
+    type: String,
+    default: null,
+  },
+});
+
+const route = useRoute();
+
+const isActiveRoute = computed(() => route.name?.startsWith(props.pagesNamePrefix));
+</script>
---
+++ src/components/layout/NavbarDropdownItem.vue
@@ -0,0 +1,26 @@
+<template>
+  <o-dropdown-item
+    tag="router-link"
+    :to="props.to"
+    aria-role="listitem"
+    :class="{'is-active-route': isActive}"
+  >
+    <slot />
+  </o-dropdown-item>
+</template>
+
+<script setup>
+import { computed } from "vue";
+import { useRoute } from "vue-router";
+
+const route = useRoute();
+
+const props = defineProps({
+  to: {
+    type: Object,
+    required: true,
+  },
+});
+
+const isActive = computed(() => route.name === props.to.name);
+</script>
